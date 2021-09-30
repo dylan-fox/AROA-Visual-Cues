8ba1d20a1a7741ff1c7fd6e188fb497fbcd251ec
Dylan Fox <dylan-fox@users.noreply.github.com>
2021-09-29 17:31:41 -0700

    HUD Cue update
    Got basic HUD cue functionality working

--- Visual Cues/Assets/HUD_Revised.cs
+++ Visual Cues/Assets/HUD_Revised.cs
@@ -51,6 +51,8 @@ public class HUD_Revised : MonoBehaviour
     void Update()
     {
         CueCast();
+        ActivateHUD();
+        ObstInfos.Clear();
     }
 
     public void CueCast()
@@ -68,15 +70,11 @@ public class HUD_Revised : MonoBehaviour
         {
             for (float yAngle = -90; yAngle <= 90; yAngle += angleInterval)
             {
-                RaycastHit hit;
-
                 //Determine new ray direction based on a certain angle off of the gaze direction
                 var rayDirection = gazeDirection;
                 rayDirection = Quaternion.AngleAxis(xAngle, Camera.main.transform.right) * rayDirection;
                 rayDirection = Quaternion.AngleAxis(yAngle, Camera.main.transform.up) * rayDirection;
                 
-
-
 
                 //Debug rays
                 if (Mathf.Abs(xAngle) <= minAngle && Mathf.Abs(yAngle) <= minAngle)
@@ -84,47 +82,139 @@ public class HUD_Revised : MonoBehaviour
                 else
                     Debug.DrawRay(headPosition, rayDirection * maxDist, Color.green);
 
+                //Cast ray
+                RaycastHit hit;
                 Physics.SphereCast(headPosition, sphereRadius, rayDirection, out hit, maxDist);
 
 
-
+                //If ray hits something, log it
                 if (hit.transform != null)
                 {
-                    Debug.Log("Hit " + hit.transform.parent.gameObject.ToString());
 
                     //If it hit an obstacle, update its information in the obstacle info list
                     bool newObst = true;
+
                     foreach (ObstInfo obst in ObstInfos)
                     {
                         if (obst.ObstObject == hit.transform.parent.gameObject)
                         {
+                            Debug.Log("Existing obstacle found: " + hit.transform.parent.gameObject.ToString());
                             newObst = false;
                             //Check if the hit has a shorter or larger distance and angle than recorded
-                            if (hit.distance > obst.ObstMaxDist)
+                            if (hit.distance >= obst.ObstMaxDist)
                                 obst.ObstMaxDist = hit.distance;
 
-                            if (hit.distance < obst.ObstMinDist)
+                            if (hit.distance <= obst.ObstMinDist)
                                 obst.ObstMinDist = hit.distance;
 
-                            //TODO: Write angle overrides
+                            //Record X and Y angles
+                            obst.ObstXAngles.Add(xAngle);
+                            obst.ObstYAngles.Add(yAngle);
 
                         }
 
                     }
 
+                    
+
+                    //Debug.Log("Onhit triggered");
                     //If the obstacle was not found, create a new one
                     if (newObst)
                     {
-                        ObstInfos.Add(new ObstInfo(hit.transform.parent.gameObject.ToString(), hit.transform.parent.gameObject, hit.distance, hit.distance, 0, 0)); //TODO: Replace 0s with Angle
+                        ObstInfos.Add(new ObstInfo(hit.transform.parent.gameObject.ToString(), hit.transform.parent.gameObject, hit.distance, hit.distance, xAngle, yAngle)); 
                         Debug.Log("New obstacle found: " + hit.transform.parent.gameObject.ToString());
                     }
-
+                    
 
                 }
 
             }
         }
 
+        //Print list of obstacles
+        Debug.Log("Number of obstacles: " + ObstInfos.Count);
+
+    }
+
+    public void ActivateHUD ()
+    {
+        //Assesses which cues should be illuminated and their appropriate size
+
+        //For each cue, scan the obstacle info list and see if it should be on or not
+        foreach (GameObject Cue in HUDCues)
+        {
+            //Debug.Log("Cue name: " + Cue.name);
+            //Cues should be off by default
+            Cue.SetActive(false);
+
+            foreach (ObstInfo obst in ObstInfos)
+            {
+                //Calculate minimum, maximum, and absolute minimum X and Y angles for each obstacle
+                float minX = 1000;
+                float minY = 1000;
+                float maxX = -1000;
+                float maxY = -1000;
+                float absMinX = 1000;
+                float absMinY = 1000;
+
+                foreach (float x in obst.ObstXAngles)
+                {
+                    if (x <= minX)
+                        minX = x;
+
+                    if (x >= maxX)
+                        maxX = x;
+
+                    if (Mathf.Abs(absMinX) <= Mathf.Abs(x))
+                        absMinX = Mathf.Abs(x);
+                }
+
+                foreach (float y in obst.ObstYAngles)
+                {
+                    if (y <= minY)
+                        minY = y;
+
+                    if (y >= maxY)
+                        maxY = y;
+
+                    if (Mathf.Abs(absMinY) <= Mathf.Abs(y))
+                        absMinY = Mathf.Abs(y);
+                }
+
+                Debug.Log("Obstacle name: " + obst.ObstName + "; min X: " + minX + "; max X: " + maxX + "; absMinX: " + absMinX);
+
+                //Check that absolute value of X or Y exceed the min angle
+                if (absMinX > minAngle || absMinY > minAngle)
+                {
+                    //Check for each HUD cue portion whether or not to light up
+                    if (Cue.name == "HUD Cue East" && maxY > minAngle)
+                    {
+                        Cue.SetActive(true);
+                        Debug.Log("East cue on.");
+                    }
+
+                    else if (Cue.name == "HUD Cue South" && maxX > minAngle)
+                    {
+                        Cue.SetActive(true);
+                        Debug.Log("South cue on.");
+                    }
+
+                    else if (Cue.name == "HUD Cue West" && minY < minAngle * -1)
+                    {
+                        Cue.SetActive(true);
+                        Debug.Log("West cue on.");
+                    }
+
+                    else if (Cue.name == "HUD Cue North" && minX < minAngle * -1)
+                    {
+                        Cue.SetActive(true);
+                        Debug.Log("North cue on.");
+                    }
+                }
+
+
+            }
+        }
 
     }
 
@@ -134,26 +224,36 @@ public class HUD_Revised : MonoBehaviour
         public GameObject ObstObject { get; set; }
         public float ObstMinDist { get; set; }
         public float ObstMaxDist { get; set; }
-        public float ObstMinAngle { get; set; }
-        public float ObstMaxAngle { get; set; }
+        public List<float> ObstXAngles { get; set; }
+        public List<float> ObstYAngles { get; set; }
+        /*
+         * public float ObstMinAngleX { get; set; }
+        public float ObstMaxAngleX { get; set; }
+        public float ObstMinAngleY { get; set; }
+        public float ObstMaxAngleY { get; set; }
+        */
         public ObstInfo()
         {
             ObstName = "unknown";
             ObstObject = null;
             ObstMinDist = 0;
             ObstMaxDist = 0;
-            ObstMinAngle = 0;
-            ObstMaxAngle = 0;
+            //ObstMinAngleX = 0;
+            //ObstMinAngleY = 0;
+            ObstXAngles = null;
+            ObstYAngles = null;
         }
 
-        public ObstInfo (string obstName, GameObject obstObject, float minDist, float maxDist, float minAngle, float maxAngle)
+        public ObstInfo (string obstName, GameObject obstObject, float minDist, float maxDist, float angleX, float angleY)
         {
             ObstName = obstName;
             ObstObject = obstObject;
             ObstMinDist = minDist;
             ObstMaxDist = maxDist;
-            ObstMinAngle = minAngle;
-            ObstMaxAngle = maxAngle;
+            ObstXAngles = new List<float>();
+            ObstYAngles = new List<float>();
+            ObstXAngles.Add(angleX);
+            ObstYAngles.Add(angleY);
         }
 
 
